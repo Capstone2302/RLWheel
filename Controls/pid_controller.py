7cf832cf28b9a42655e3deb1e08abc5660a92cc4
Ashli <forbesashli@gmail.com>
2024-03-30 20:41:45 -0700

    wheel PID mostly implemented

--- Controls/pid_controller.py
+++ Controls/pid_controller.py
@@ -38,7 +38,17 @@ class MotorController:  # add class definitions
         self.encoder.set_center_pos()
         print(self.encoder.curr_pos_rad)
 
-    def control_routine(self, ball_pos, log):
+    def receive_delt_enc(self):
+        return receive_msg()
+
+    def send_pwm_val(self,pwm_val):
+        msg = str(int(pwm_val)).ljust(7, "\t")
+        if send_msg(msg):
+            return True
+        else:
+            return False
+
+    def control_routine(self, curr_pos, log):
         current_time = time.time()
         dt = current_time - self.start_time
         self.start_time = current_time
@@ -68,30 +78,58 @@ class MotorController:  # add class definitions
             time.sleep(1)
             print("stabalized")
 
-    def PID_mode(self, curr_pos):
+    def BallPosPID(self, curr_pos):
+        # get encoder value from UART
+        curr_time = time.time()
+        diff_time = curr_time - self.start_time
+        self.start_time = curr_time
+        diff_wheel_pos = self.encoder.delt_to_rad(receive_msg())
+
+        diff_pos = 0 - curr_pos  # set_rpm - curr_rpm
+
+        # using PID variables and such, calculate PWM output
+        self.integrator_val += self.e_prev * diff_time
+
+        pwm_kp = self.k_p * diff_pos
+        pwm_ki = self.k_i * (self.integrator_val)
+        pwm_kd = self.k_d * (diff_pos - self.e_prev) / diff_time
+        pwm_est = pwm_kp + pwm_ki + pwm_kd
+        self.e_prev = diff_pos
+
+        msg = str(int(-pwm_est)).ljust(7, "\t")
+        send_msg(msg)
+
+    def WheelPosPID(self, set_pos, log):
+        k_p = 175
+        k_i = 100
+        k_d = 0
         # get encoder value from UART
         curr_time = time.time()
         diff_time = curr_time - self.start_time
         self.start_time = curr_time
         delt_enc =receive_msg()
         curr_pos = self.encoder.delt_to_rad(delt_enc)
-        diff_pos = curr_pos-set_pos
         # using PID variables and such, calculate PWM output
         self.integrator_val += self.e_prev * diff_time
+        error = set_pos - curr_pos
+        if abs(error) > np.pi:
+            error = error - 2 * np.pi * np.copysign(1, error)
+
+        diff_pos = error
 
         pwm_kp = k_p * diff_pos
         pwm_ki = k_i * (self.integrator_val)
-        pwm_kd = k_d * (diff_pos - self.e_prev) / diff_time
+        pwm_kd = k_d * (self.e_prev - diff_pos) / diff_time
         pwm_est = pwm_kp + pwm_ki + pwm_kd
         self.e_prev = diff_pos
 
-        msg = str(int(-pwm_est)).ljust(7, "\t")
+        msg = str(int(pwm_est)).ljust(7, "\t")
         send_msg(msg)
         if log:
             self.logger.log_data(
                 delt_enc,
                 diff_time,
-                0,
+                curr_pos,
                 set_pos,
                 curr_time,
                 pwm_kp,
@@ -100,49 +138,9 @@ class MotorController:  # add class definitions
                 0,
             )
 
-    def PWM_Response_test(self, pwm_val, log):
-        # get encoder value from UART
-        delt_enc = receive_msg()
-        curr_time = time.time()
-        diff_time = curr_time - self.start_time
-        self.start_time = curr_time
-
-        curr_rpm = (delt_enc * 60) / (diff_time * 2400)  # CCW is positive
-        # send messages over UART
-        msg = str(int(pwm_val)).ljust(7, "\t")
-        send_msg(msg)
-        print(curr_rpm)
-        if log:
-            self.logger.log_data(
-                delt_enc,
-                diff_time,
-                curr_rpm,
-                0,
-                curr_time,
-                pwm_val,
-                0,
-                0,
-                0,
-            )
-
-    def PID_response_test1(self, max, log_perhaps):
-        # continuous tests
-        for i in range(max, -max, 1):
-            self.control_routine(i / 10, log_perhaps)
-
-        for i in range(-max, max, 1):
-            self.control_routine(i / 10, log_perhaps)
-
-    def PID_response_test2(self, max, square_len, log_perhaps):
-        # step response tests
-        for i in range(max * 6):
-            if i % square_len < square_len / 2:
-                self.control_routine(max, log_perhaps)
-            else:
-                self.control_routine(-max, log_perhaps)
-
     def exit(self, log):
         msg = str(0).ljust(7, "\t")
         send_msg(msg)
         if log:
+            print("trying to write")
             self.logger.write_file()
